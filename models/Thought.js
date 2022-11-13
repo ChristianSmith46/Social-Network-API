840b0174cb9368aac8622f87afb98cb9a90e23ad
Christian Smith <christian.46.smith@gmail.com>
2022-11-13 14:46:16 -0500

    Created schemas and models

---
+++ models/Thought.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => {
+            return date.localeTimeZone();
+        }
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [Reaction]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Add reactionCount by getting reactions list length
+thoughtSchema
+  .virtual('reactionCount')
+  // Getter
+  .get(function () {
+    return this.reactions.length;
+  });
+
+// Initialize our User model
+const Thought = model('thought', userSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require('mongoose');
+
+// Email validation found here https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+const userSchema = new Schema(
+  {
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: { 
+        type: String,
+        unique: true,
+        required: true,
+        // Email validation found here https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Add frienCount by getting friend list length
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  });
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
